8950a262f94582a08cce3fd765fabd788eece7e8
Biel Simon Rojas <bielsimon2@gmail.com>
2019-01-16 20:30:17 +0100

    Updated prog.js

--- prog.js
+++ prog.js
@@ -5,7 +5,7 @@ const bindings = {
   }
 };
 const changeBinding = (on, to) => bindings[on] = to;
-const binding = (on, to) => bindings[on];
+const binding = (on) => bindings[on];
 
 String.prototype.binding = function () {
   return binding(this);
@@ -21,25 +21,27 @@ String.prototype.bindTo = function (to, _in) {
 function withInitDo(args, program) {
   let bindStructure = (array, transform, inner) => {
     return array.reduceRight((pv, cv, i, arr) => {
-      console.log(`cv`, cv);
-      return cv.key.bindTo(transform(cv, arr[i + 1]), () => pv)
+      if(typeof cv === "string") cv = [cv];
+      let next = null;
+      if(i < arr.length - 1) next = arr[i + 1]
+      return cv[0].bindTo(transform(cv, next), () => pv)
       // return bindTo(cv.key, transform(cv.value, arr[i + 1]), pv)
     }, inner)
   }
-  let bindArgs = (arr, inner) => bindStructure(arr, a => a, inner)
+  let bindArgs = (arr, inner) => bindStructure(arr, (a, next) => a[1], inner)
   let instructionTransform = (instruction, next) => {
-    if (next == null) next = {key: '#RETURN', value: '#RETURN'.binding()}
+    if (next == null) next = ['#RETURN', '#RETURN'.binding()]
     return () => {
-      console.log(`Executing ${instruction.key}`, instruction);
-      instruction.value();
-      console.log(`Going from ${instruction.key} to ${next.key}`, next);
-      next.key.binding()()
+      console.log(`Executing ${instruction[0]}`);
+      instruction[1]();
+      // console.log(`Going from ${instruction[0]} to ${next[0]}`, next);
+      next[0].binding()()
     }
     // return `[ ${instruction} value. ${next.key} binding value${next.value === 'nil' ? ': ' + next.value : ''} ]`
   }
   let bindProgram = (arr, inner) => bindStructure(arr, instructionTransform, inner)
   //`[ ${program[0].key} binding value ]`
-  let compiled = bindArgs(args, bindProgram(program, () => program[0].key.binding()()))
-  while (typeof compiled === 'function') compiled = compiled();
+  let compiled = bindArgs(args, bindProgram(program, () => program[0][0].binding()()))
+  while (true) compiled = compiled();
 }
 module.exports = {withInitDo}
