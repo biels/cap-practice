6462970d44d987652f8a209abe01566692a1b2e9
Biel <bielsimon2@gmail.com>
2019-01-18 18:48:10 +0100

    Added fibonacci example

---
+++ example4.js
@@ -0,0 +1,21 @@
+const PROG = require('./prog')
+
+
+let args = [['#n', 11], ['#fib', 1], ["#prevFib", 1], ['#i', 2], ['#temp', 0]]
+let prog = [
+  [ '#label1', () => {
+    if('#n'.binding() <= 1) '#RETURN'.binding()('#n'.binding())
+  } ],
+  ['#loop', () => {
+    if('#i'.binding() === '#n'.binding())'#endloop'.binding()();
+  }],
+  [ '#label2', () => '#temp'.changeBinding('#fib'.binding())],
+  [ '#label3', () => '#fib'.changeBinding('#fib'.binding() + '#prevFib'.binding())],
+  [ '#label4', () => '#prevFib'.changeBinding('#temp'.binding())],
+  [ '#label5', () => '#i'.changeBinding("#i".binding() + 1)],
+  [ '#label6', () => '#loop'.binding()()],
+  [ '#endloop', () => {}],
+  [ '#label7', () => '#RETURN'.binding()('#fib'.binding())],
+]
+
+PROG.withInitDo(args, prog)
